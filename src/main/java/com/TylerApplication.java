fce2019254804117f0db529b731361d28cd80fcd
noahwenck <123976574+noahwenck@users.noreply.github.com>
2025-04-12 15:36:07 -0500

    init

---
+++ src/main/java/com/TylerApplication.java
@@ -0,0 +1,12 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TylerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TylerApplication.class, args);
+    }
+}
